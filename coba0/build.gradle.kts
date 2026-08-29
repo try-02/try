bbf5757141cc0cb84874c0401d90f920dcd0b7f1
b3bas <benas@gmail.com>
2026-08-29 19:37:42 +0700

    update

--- coba0/build.gradle.kts
+++ coba0/build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file
 plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
