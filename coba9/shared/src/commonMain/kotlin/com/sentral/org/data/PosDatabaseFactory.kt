c26539f677c4074814b424480dedf2a85c239ed2
b3bas <benas@gmail.com>
2026-09-03 16:28:41 +0700

    update

--- coba9/shared/src/commonMain/kotlin/com/sentral/org/data/PosDatabaseFactory.kt
+++ coba9/shared/src/commonMain/kotlin/com/sentral/org/data/PosDatabaseFactory.kt
@@ -2,7 +2,6 @@ package com.sentral.org.data
 
 import androidx.room3.RoomDatabase
 import kotlinx.coroutines.Dispatchers
-import androidx.room3.migration.Migration
 import com.sentral.org.data.migrasi.PosMigrasi
 /**
  * Membangun instance PosDatabase dari [RoomDatabase.Builder] yang sudah
@@ -20,16 +19,4 @@ fun createPosDatabase(builder: RoomDatabase.Builder<PosDatabase>): PosDatabase =
     builder
         .setQueryCoroutineContext(Dispatchers.Default)
         .addMigrations(*PosMigrasi.ALL.toTypedArray())
-        // JANGAN PERNAH pakai fallbackToDestructiveMigration() di aplikasi POS!
-        // Sebagai gantinya, kita pakai fallback yang aman: backup DB lama + 
-        // buat DB baru jika migration gagal. Ini mencegah data loss total.
-        .setMigrationFallback { connection: SQLiteConnection ->
-            // TODO (tahap hardening): implement backup otomatis DB lama
-            // sebelum membuat DB baru. Sementara biarkan throw agar developer
-            // sadar ada migration path yang hilang.
-            throw IllegalStateException(
-                "Migration path tidak ditemukan. Ini adalah bug developer, " +
-                "bukan error user. Tambahkan migration di PosMigrations.ALL"
-            )
-        }
         .build()
