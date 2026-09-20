01d104d7e5f3816b5eab36e703627e31f56a8536
b3bas <benas@gmail.com>
2026-09-20 11:24:39 +0700

    update

--- coba9/app/src/main/java/com/sentral/org/ui/screen/shift/TutupShiftScreen.kt
+++ coba9/app/src/main/java/com/sentral/org/ui/screen/shift/TutupShiftScreen.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
@@ -159,7 +159,7 @@ fun TutupShiftScreen(
 }
 
 @Composable
-private fun PanelBlindCount(
+private fun ColumnScope.PanelBlindCount(
     namaKasir: String,
     dimulaiPada: Long,
     kasAktualNominal: Long,
@@ -279,7 +279,7 @@ private fun PanelBlindCount(
 }
 
 @Composable
-private fun PanelRekonsiliasi(
+private fun ColumnScope.PanelRekonsiliasi(
     summary: ShiftSummary,
     catatan: String,
     onCatatanChange: (String) -> Unit,
