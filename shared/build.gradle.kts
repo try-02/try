81920118c9e4a9ab22c52252a50aa91b4a11767e
b3bas <benas@gmail.com>
2026-09-26 11:24:05 +0700

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.room3)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.compose)
 }
 
 // Konfigurasi Room Auto-Migration Schema (sekarang di :shared)
